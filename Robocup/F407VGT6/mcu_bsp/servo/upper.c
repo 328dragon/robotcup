6d22140446f1f49e029bc68c4c3d0e2a2f95277a
Nagisa <2964793117@qq.com>
2025-08-08 15:56:07 +0800

    封装好了上层逻辑

--- Robocup/F407VGT6/mcu_bsp/servo/upper.c
+++ Robocup/F407VGT6/mcu_bsp/servo/upper.c
@@ -1,2 +1,114 @@
 #include "upper.h"
+/*
+    * @brief 获得颜色任务
+*/
+void GetColorTask(Color_t* color_task, int* color_task_index)
+{
+    // 创建颜色映射表，使用Color_t枚举
+    static const Color_t colorMap[16][5] = {
+        /* 1  */ {BLACK, WHITE, RED, GREEN, BLUE},
+        /* 2  */ {WHITE, BLACK, RED, GREEN, BLUE},
+        /* 3  */ {WHITE, BLACK, GREEN, RED, BLUE},
+        /* 4  */ {BLUE, WHITE, BLACK, RED, GREEN},
+        /* 5  */ {WHITE, RED, BLUE, BLACK, GREEN},
+        /* 6  */ {BLACK, RED, BLUE, WHITE, GREEN},
+        /* 7  */ {BLUE, GREEN, BLACK, WHITE, RED},
+        /* 8  */ {GREEN, WHITE, BLUE, BLACK, RED},
+        /* 9  */ {WHITE, GREEN, BLACK, BLUE, RED},
+        /* 10 */ {BLACK, RED, BLUE, GREEN, WHITE},
+        /* 11 */ {RED, BLUE, GREEN, BLACK, WHITE},
+        /* 12 */ {GREEN, RED, BLACK, BLUE, WHITE},
+        /* 13 */ {WHITE, RED, BLUE, GREEN, BLACK},
+        /* 14 */ {RED, GREEN, WHITE, BLUE, BLACK},
+        /* 15 */ {BLUE, WHITE, GREEN, RED, BLACK},
+        /* 16 */ {GREEN, BLUE, RED, WHITE, BLACK}
+    };
 
+    // 检查输入数字是否有效
+    if (*color_task_index >= 1 && *color_task_index <= 16) 
+    {
+        // 将对应行的颜色复制到输出数组
+        for (int i = 0; i < 5; i++) 
+        {
+            color_task[i] = colorMap[*color_task_index - 1][i];
+        }
+    } 
+}
+
+/*
+    * @brief 绑定料盘槽的信息
+*/
+void DistributionLoop(Servo_t* servos,ThingStore_t* plate_things,Color_t* current_color_ptr, UpperTaskFlag* upperflag, int* CurrentColorLoop)
+{
+    if(*CurrentColorLoop<=5)
+        if (*upperflag == PICKINGIN)
+        {
+            Servo_SetAngle(&servos[0], PICK_LEFT);
+            Servo_SetAngle(&servos[1], PICK_DOWN);
+            vTaskDelay(1000); // 等待舵机转动完成，需要实测
+
+            // 此处还需加入吸盘启动
+
+            vTaskDelay(500);
+            Servo_SetAngle(&servos[0], FIND_PLATE);
+            Servo_SetAngle(&servos[1], UP);
+            vTaskDelay(500);
+            Servo_SetAngle(&servos[1], COLORTASKHEIGHT);
+            vTaskDelay(500);
+            *upperflag = GETCOLORIN;
+        }
+        if (*upperflag == GETCOLORIN)
+        {
+            plate_things[*CurrentColorLoop]._color = *current_color_ptr;
+            plate_things[*CurrentColorLoop]._angle = THING_GIMBAL_ORIGIN_ANGLE + *CurrentColorLoop*THING_GIMBAL_FIXED_DELTA;
+            plate_things[*CurrentColorLoop]._number = *CurrentColorLoop;
+            (*CurrentColorLoop)++;
+            *upperflag = PUTINGIN;
+        }
+        if (*upperflag == PUTINGIN)
+        {
+            Servo_SetAngle(&servos[1], PUT_DOWN);
+            vTaskDelay(500);
+
+            // 此处还需加入吸盘关闭
+
+            *upperflag = IDLE;
+        }
+}
+
+void PutGoal(Color_t* color_task,Servo_t* servos,ThingStore_t* plate_things, UpperTaskFlag* upperflag,int* PutGoalLoop)
+{
+    if(*PutGoalLoop<=5)
+    {   
+        if (*upperflag == PICKINGOUT)
+         // 按顺序筛选对应颜色任务的料盘
+        {
+            for (int i = 0; i < 6; i++) 
+            {
+                if (plate_things[i]._color == color_task[*PutGoalLoop])   
+                {
+                    Servo_SetAngle(&servos[2], plate_things[i]._angle);
+                }
+            }
+            Servo_SetAngle(&servos[0], FIND_PLATE);
+            Servo_SetAngle(&servos[1], PUT_DOWN);
+            vTaskDelay(1000); // 等待舵机转动完成，需要实测
+
+            // 此处还需加入吸盘启动
+
+            Servo_SetAngle(&servos[1], UP);
+        }
+
+        // 此处还需等待底盘移动到目标位置
+
+        if(*upperflag == PUTTINGOUT)
+        {
+            Servo_SetAngle(&servos[0], GOAL);
+
+            // 此处还需加入吸盘关闭
+
+            vTaskDelay(500);
+        }
+
+    }
+}
--- Robocup/F407VGT6/mcu_bsp/servo/upper.h
+++ Robocup/F407VGT6/mcu_bsp/servo/upper.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: Nagisa 2964793117@qq.com
+ * @Date: 2025-08-07 22:06:30
+ * @LastEditors: Nagisa 2964793117@qq.com
+ * @LastEditTime: 2025-08-08 15:55:10
+ * @FilePath: \MDK-ARMd:\project\git\robotcup\Robocup\F407VGT6\mcu_bsp\servo\upper.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef _UPPER_H_
 #define _UPPER_H_
 /*
@@ -5,19 +13,14 @@
 */
 #include "main.h"
 #include "servo.h"
+#include "FreeRTOS.h"
+#include "task.h"
 //所有舵机的常量或枚举常量
-#define THING_GIMBAL_FIXED_DELTA
-#define ASS_SERVO_OPEN
-#define ASS_SERVO_CLOSE
-/*
-    * @brief 抬升舵机枚举常量
-*/
-typedef enum
-{
-    PICK_DOWN,
-    UP,
-    PUT_DOWN
-} Lift_Servoangle_t;
+#define THING_GIMBAL_FIXED_DELTA 60
+#define THING_GIMBAL_ORIGIN_ANGLE 60
+// #define ASS_SERVO_OPEN
+// #define ASS_SERVO_CLOSE
+
 /*
     * @brief 前云台枚举常量
 */
@@ -27,6 +30,16 @@ typedef enum
     FIND_PLATE,
     GOAL
 } GimbalArm_Servoangle_t;
+/*
+    * @brief 抬升舵机枚举常量
+*/
+typedef enum
+{
+    PICK_DOWN,
+    UP,
+    COLORTASKHEIGHT,
+    PUT_DOWN
+} Lift_Servoangle_t;
 /*
     * @brief 物块颜色枚举常量
 */
@@ -44,14 +57,39 @@ typedef enum
 */
 typedef struct
 {
-    int _color;
+    Color_t _color;
     int _angle;
     int _number;
 }ThingStore_t;
-// 用全局数组存储记忆内容，功能函数全部尽量使用指针操作
-void DistrubutionLoop(ThingStore_t* plate_things,Color_t* current_color_ptr);
-void ThingGimbalControl(ThingStore_t* plate_things,GimbalArm_Servoangle_t* gimbal_angle);
-int PickStoreLoop();
-int PickPutLoop();
+
+/*
+    * @brief 上层机构状态机
+*/
+typedef enum
+{
+    PICKINGIN,
+    GETCOLORIN,
+    PUTINGIN,
+    PICKINGOUT,
+    PUTTINGOUT,
+    IDLE
+} UpperTaskFlag;
+// 打算servo数组一共3个舵机，第一个舵机控制前云台，第二个控制升降，第三个控制转盘
+
+// 用全局数组存储记忆内容，功能函数全部使用指针操作
+
+// 确定有舵机数组全局数组，Servo_t servos;
+// 确定有料盘槽数组全局数组，ThingStore_t g_plate_things;
+// 本局任务的颜色顺序全局数组 Color_t color_task[6]
+
+// 确定当前颜色传感器颜色全局变量，Color_t g_current_color_ptr;
+// 确定当前状态机标志全局变量，UpperTaskFlag g_upperflag;
+// 确定本局任务的颜色对应数字全局变量，int color_task_index;
+// 确定有当前颜色识别轮数全局变量, int CurrentColorLoop;
+// 确定当前放置好的物块任务轮数，int PutGoalLoop;
+
+void GetColorTask(Color_t* color_task, int* color_task_index);
+void DistributionLoop(Servo_t* servos,ThingStore_t* plate_things,Color_t* current_color_ptr, UpperTaskFlag* upperflag,int* CurrentColorLoop);
+void PutGoal(Color_t* color_task,Servo_t* servos,ThingStore_t* plate_things, UpperTaskFlag* upperflag,int* PutGoalLoop);
 
 #endif
